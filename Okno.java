86b0c61ca67a61d0f6c56c74c0f5ecd7fff7e028
Hitoshiq <98221146+Hitoshiq@users.noreply.github.com>
2022-01-22 19:00:49 +0300

    Окно
    Нечего не зделали

--- Okno.java
+++ Okno.java
@@ -2,20 +2,23 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Okno extends JPanel implements ActionListener{
     int WIDTH = 1000;
     int HEIGHT = 1000;
     int CELL = 50;
-    int DELAY = 60;
+    int DELAY = 120;
     boolean play;
     Timer timer;
-    Snake snake = new Snake(CELL, Color.BLACK, CELL, 'R', 100, 100);
+    Snake snake = new Snake(CELL, Color.BLACK, CELL, 'R', 100, 100, WIDTH, HEIGHT);
 
     public Okno(){
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setFocusable(true);
         this.setBackground(Color.yellow);
+        this.addKeyListener(new MyKeyAdapter());
         start();
     }
 
@@ -40,13 +43,34 @@ public class Okno extends JPanel implements ActionListener{
         snake.draw(g);
     }
 
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            // 'R' - Right, 'L' - Left, 'U' - Up, 'D' - Down
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_W -> {
+                    if (snake.direction != 'D') snake.direction = 'U';
+                }
+                case KeyEvent.VK_A-> {
+                    if (snake.direction != 'R') snake.direction = 'L';
+                }
+                case KeyEvent.VK_D-> {
+                    if (snake.direction != 'L') snake.direction = 'R';
+                }
+                case KeyEvent.VK_S-> {
+                    if (snake.direction != 'U') snake.direction = 'D';
+                }
+            }
+        }
+    }
+
     /**
      * Обновляется каждый кадр, следит за происходящим в игре
      */
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        snake.move();
+        snake.checkWalls();
         repaint(); // перерисовывает все объекты в игре
     }
 }
-
