136ace8e57ea207e250e15aa4f9285eee322db5b
Hitoshiq <98221146+Hitoshiq@users.noreply.github.com>
2022-01-22 17:56:28 +0300

    Рисуем змейку
    Создали метод, который прорисовывает голову змеи

--- Snake.java
+++ Snake.java
@@ -2,13 +2,30 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class Snake {
-    int SIZE = new Okno().CELL;
-    Color COLOR = Color.BLACK;
+    int SIZE;
+    Color COLOR;
     ArrayList<Snake> bodyParts = new ArrayList<>();
     int x;
     int y;
-    int SPEED = 1;
-    int DIRECTION;
+    int SPEED;
+    char direction;
+
+    public Snake(int cell, Color color, int speed, char direction, int x, int y){
+        this.SIZE = cell;
+        this.COLOR = color;
+        this.SPEED = speed;
+        this.direction = direction;
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Рисует змейку
+     * @param g объект, который рисует змею
+     */
+    public void draw(Graphics g){
+        g.fillRect(this.x, this.y, this.SIZE, this.SIZE);
+    }
 
     /**
      * Змейка двигается на 1 блок
@@ -19,4 +36,7 @@ public class Snake {
      * Меняем направление движения в случае нажатия кнопок на клавиатуре
      */
     public void changeDirection(){}
+
+
 }
+
